cd1588716996683800c0398586aa54460b3bfe82
Fer Bendala <ferbendala@MBP-de-Fer.home>
2023-07-12 09:45:24 +0200

    separating scraping in modules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-const { getFormattedDate } = require( './utils/date' )
-const { writeJson } = require( './utils/file' )
-const { scrapeSupermarket } = require( './modules/scraping' )
-const { getProducts } = require( './modules/get-products' )
+const fs = require( 'fs' );
+const path = require( 'path' );
+const { getFormattedDate } = require( './utils/date' );
+const { writeJson } = require( './utils/file' );
+const { scrapeSupermarket } = require( './modules/scraping' );
+const { getProducts } = require( './modules/get-products' );
 
 const main = async () => {
-    const url = 'https://tienda.mercadona.es/'
-    const postalCode = '08028'
-    const document = await scrapeSupermarket( url, postalCode )
-    const products = getProducts( document )
+    const url = 'https://tienda.mercadona.es/';
+    const postalCode = '08028';
+    const document = await scrapeSupermarket( url, postalCode );
+    const products = getProducts( document );
 
-    await writeJson( products, `./data/products-${getFormattedDate()}.json` )
-}
+    const dataFolder = path.join( __dirname, 'data' );
+    if ( !fs.existsSync( dataFolder ) ) {
+        fs.mkdirSync( dataFolder );
+    }
 
-main().catch( console.error )
+    const filename = `products-${getFormattedDate()}.json`;
+    const filePath = path.join( dataFolder, filename );
+
+    await writeJson( products, filePath );
+};
+
+main().catch( console.error );
